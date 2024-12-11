74aaf53151ed5ed201f4e152a93e6e9891f565bb
mukthikanth <Mukthikanth04@gmail.com>
2024-12-11 05:07:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //modern js is used
+
+ //alert( 3 + 3) won't work because we are using nodejs and not browser
+console.log(3 + 3);
+console.log('Mukthikanth');
+// Code readability should be high so keep it simple and clean
+
+let name = "Mukthikanth"
+let age = 18
+let userLoggedin = true
+let state; // it is undefined where variable is defined but value is not defined
+let city = null // null is empty
+
+// number => 13
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable decided but value not defined
+// symbols => unique
+
+// object 
+
+console.log(typeof name); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+

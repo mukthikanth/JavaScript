fcd2b8163aab7f70046957eb71eb787a8cef13bb
mukthikanth <Mukthikanth04@gmail.com>
2024-12-11 07:11:43 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,13 +1,13 @@
 let score = false
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let ValueInNumber = Number(score)
 
-console.log(ValueInNumber); // Output is NaN as score was "33abc" which means not a number
+// console.log(ValueInNumber); // Output is NaN as score was "33abc" which means not a number
 
-console.log(typeof ValueInNumber);
+// console.log(typeof ValueInNumber);
 
 // for boolean true Number is 1 and for false it is 0
 // "33" => 33; type: number
@@ -17,7 +17,7 @@ console.log(typeof ValueInNumber);
 
 let LoggedIn = "1"
 let BooleanLoggedIn = Boolean(LoggedIn)
-console.log(BooleanLoggedIn);
+// console.log(BooleanLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -25,8 +25,52 @@ console.log(BooleanLoggedIn);
 
 let number = 33
 let stringnumber = String(number)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+// ************* Operations ***********
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Mukthikanth"
+let str3 = str1 + str2
+console.log(str3);
+
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log(1 + 2);
+// console.log(1 + 1 + "2");
+// console.log("1" + (2 + 2));
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+// console.log(+"a");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++ // postfix
+// ++gamecounter prefix
+console.log(gamecounter);
+
+
+
+
 
 
 

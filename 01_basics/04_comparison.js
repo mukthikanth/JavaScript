37e499208fb519b9da50c8c2c6ff5236cdfc8079
mukthikanth <Mukthikanth04@gmail.com>
2024-12-11 14:10:33 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// === refers to strict check which also checks the data type
+
+console.log("2" === 2);

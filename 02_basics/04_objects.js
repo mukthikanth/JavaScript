fca96a54631c01f3f1644b04dfd8a1d2a7361760
mukthikanth <Mukthikanth04@gmail.com>
2024-12-21 11:45:28 +0000

    object de-structure and json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -30,14 +30,14 @@ const obj3 = {5:"a", 6:"b"}
 //const obj4 = {obj1, obj2, obj3} // This is like push in arrays it adds an entire object inside a object
 //const obj4 = Object.assign({}, obj1, obj2, obj3)// works like concat in arrays
 
-const obj4 = {...obj1, ...obj2, ...obj3} // shatter method
+const obj4 = {...obj1, ...obj2, ...obj3} // shatter method is the best method
 //console.log(obj4);
 
 //console.log(Object.keys(tinderUser));// the output is in array and can further be used in loops
 //console.log(Object.values(tinderUser));// the output is in array and can further be used in loops
 //console.log(Object.entries(tinderUser));// everything to array first value is key and second one is to value
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));// to check if the key exists or not
-console.log(tinderUser.hasOwnProperty('isLogged'));
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));// to check if the key exists or not
+//console.log(tinderUser.hasOwnProperty('isLogged'));
 
 
 
@@ -60,3 +60,25 @@ const users = [
 
 //console.log(users[1].email);
 
+// object de-structure and JSON API
+
+
+const course = {
+    courseName: "Spirituality",
+    courserPirce: "3 niggas",
+    courseInstructor: "Jesus"
+}
+
+// const {courseInstructor} = course
+// console.log(courseInstructor);// output is jesus
+
+const {courseInstructor: Instructor} = course
+console.log(Instructor);
+
+// JSON API
+// {
+//     "name": "Mukthikanth",
+//     "age": 18,
+//     "email": "muk@gmail.com"
+
+// }
